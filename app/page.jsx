6199308c45e385d749ce0a9551fa210f53ee15e7
darylcklau <daryl.ck.lau@gmail.com>
2026-04-22 15:20:07 +0800

    Add Home component for main page layout

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:"40px",fontFamily:"Arial"}}>
+      <h1>BIG Bulk Beta</h1>
+      <p>Your app is live.</p>
+    </main>
+  )
+}
